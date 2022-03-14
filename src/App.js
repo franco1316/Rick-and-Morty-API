227767d97871bc15ec21c0a2f69ab107d1588636
franco1316 <francoacm1011000@gmail.com>
2022-03-14 15:42:29 -0500

    better styles

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
   }, [] )
 
   return (
-    <div className = " App " >
+    <div className = " App flex-column" >
       <SearchBox setLocation = { setLocation } />
       <ResidentList location={location} residents={location.residents}/>
     </div>
